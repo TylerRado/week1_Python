f67ae28f6c773dca1142862f6d8d4296718626ae
TylerRado <radothando@gmail.com>
2024-10-30 17:36:44 +0200

    Completed

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# Input function to ask the user for their name and store it in a variable called "name".
+
+name = input("Enter your name:")
+
+# Input function to ask the user for their age and store it in a variable called "age"
+
+
+age = int(input("Enter your Age:"))
+
+# Input function to as user for their location and store it in a variable called "location"
+
+location = input("Enter your Location:")
+ 
+print(f'Hello {name} you are {age} years old and live in {location}')
